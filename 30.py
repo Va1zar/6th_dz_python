4cca585d19f82ef4dd58dd8c373403e8cfa0231b
Evgeniy <s.h.a.r.k.112@mail.ru>
2023-03-07 20:14:20 +0700

    solve of 30

--- 30.py
+++ 30.py
@@ -2,3 +2,12 @@
 # Её первый элемент, разность и количество элементов нужно ввести с клавиатуры.
 # Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 # Каждое число вводится с новой строки.
+
+# Ввод: 7 2 5
+# Вывод: 7 9 11 13 15
+
+first_element = int(input("Input 1st element of the list: "))
+step = int(input("Input step: "))
+my_list_length = int(input("Input length of our list: "))
+for i in range(my_list_length):
+    print(first_element + i*step)
